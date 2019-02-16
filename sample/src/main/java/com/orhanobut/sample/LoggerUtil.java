7883c50abf72fa1a5b3bd5d002abd050df7e12d4
jacky <1165002248@qq.com>
2019-02-16 15:01:35 +0800

    Add logger active profile at LoggerUtil.java by jacky.

--- sample/src/main/java/com/orhanobut/sample/LoggerUtil.java
+++ sample/src/main/java/com/orhanobut/sample/LoggerUtil.java
@@ -1,4 +1,12 @@
 package com.orhanobut.sample;
 
 public class LoggerUtil {
+
+    public enum LOGGER_ACTIVE_PROFILE {
+        LOCAL, DEVELOP, PRODUCT
+    }
+    public static void init(LOGGER_ACTIVE_PROFILE profile)
+    {
+
+    }
 }
